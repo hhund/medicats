c842821007eedcad350f6eb935d221c22ba69564
hhund <hauke.hund@hs-heilbronn.de>
2016-09-05 17:06:14 +0200

    added German/English copyright notices and missing produces annotation

--- medicats-parent/medicats-web/src/main/java/de/gecko/medicats/web/rest/MedicatsWebservice.java
+++ medicats-parent/medicats-web/src/main/java/de/gecko/medicats/web/rest/MedicatsWebservice.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Link;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import de.gecko.medicats.web.transfer.MedicatsDto;
@@ -28,8 +30,12 @@ public class MedicatsWebservice
 	}
 
 	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public Response root()
 	{
-		return Response.ok(new MedicatsDto(links)).build();
+		return Response.ok(new MedicatsDto(
+				"Source for ICD-10-GM, OPS and Alpha-ID catalogs: Deutschen Instituts für Medizinische Dokumentation und Information (DIMDI) http://www.dimdi.de.",
+				"Die Erstellung erfolgt unter Verwendung der maschinenlesbaren Fassung des Deutschen Instituts für Medizinische Dokumentation und Information (DIMDI). Quelle für ICD-10-GM, OPS und Alpha-ID Kataloge: http://www.dimdi.de.",
+				links)).build();
 	}
 }
--- medicats-parent/medicats-web/src/main/java/de/gecko/medicats/web/transfer/MedicatsDto.java
+++ medicats-parent/medicats-web/src/main/java/de/gecko/medicats/web/transfer/MedicatsDto.java
@@ -3,21 +3,35 @@ package de.gecko.medicats.web.transfer;
 import java.util.Collection;
 
 import javax.ws.rs.core.Link;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 @XmlRootElement(name = "medicats")
+@XmlType(propOrder = { "copyrightEn", "copyrightDe", "links" })
 public class MedicatsDto extends WithLinks
 {
+	@XmlElement(name = "copyright-en")
+	private final String copyrightEn;
+
+	@XmlElement(name = "copyright-de")
+	private final String copyrightDe;
+
 	/**
-	 *@deprecated only for JAX-B 
+	 * @deprecated only for JAX-B
 	 */
 	@Deprecated
 	MedicatsDto()
 	{
+		copyrightEn = null;
+		copyrightDe = null;
 	}
-	
-	public MedicatsDto(Collection<? extends Link> links)
+
+	public MedicatsDto(String copyrightEn, String copyrightDe, Collection<? extends Link> links)
 	{
 		super(links);
+
+		this.copyrightEn = copyrightEn;
+		this.copyrightDe = copyrightDe;
 	}
 }
